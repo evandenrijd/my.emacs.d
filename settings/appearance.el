66dfcc628d0fa182f950d77f8a4c0250c0f7e949
van <evandenrijd@gmail.com>
2017-03-08 21:48:32 +0100

    feat(appeaarance): default font/theme

--- settings/appearance.el
+++ settings/appearance.el
@@ -1,4 +1,17 @@
 ;; Make zooming affect frame instead of buffers
 (require 'zoom-frm)
 
+;; essentially set the font-size
+(setq van/default-font "-*-*-*-*-*-*-20-*-*-*-m-0-iso10646-1")
+
+(defun use-default-theme ()
+  (interactive)
+  ;;(load-theme 'default-black)
+  (load-theme 'adwaita t)
+  (when (boundp 'magnar/default-font)
+    (set-face-attribute 'default nil :font magnar/default-font))
+  (set-face-attribute 'default nil :font van/default-font))
+
+(use-default-theme)
+
 (provide 'appearance)
